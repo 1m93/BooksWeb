90b684e1289d9bfc71d2c70ee623296ffc238288
Nguyễn Tuấn Linh <43125994+1m93@users.noreply.github.com>
2019-05-12 02:49:50 +0700

    Update config.php

--- BooksParadiseAdmin/modules/config.php
+++ BooksParadiseAdmin/modules/config.php
@@ -1,8 +1,7 @@
 <?php
-    $tenmaychu = "localhost";
-    $tentaikhoang = "root";
-    $matkhau = "";
-    $dbname = "booksparadise";
+    $tenmaychu = "ec2-174-129-240-67.compute-1.amazonaws.com";
+    $tentaikhoang = "oycjwdlalbdttb";
+    $matkhau = "893f1c375f1562ec7119752a54b8b409b52e481e92a6012b7960568d4c09ca59";
+    $dbname = "d12r12ti6slhab";
     $conn = mysqli_connect($tenmaychu,$tentaikhoang,$matkhau,$dbname) or die ('không kết nối được');
-    mysqli_set_charset($conn, 'UTF8');
 ?>
